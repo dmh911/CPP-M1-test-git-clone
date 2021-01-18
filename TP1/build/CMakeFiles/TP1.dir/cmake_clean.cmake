f5d8008a5278b1401682fa3bdf570b5eda7596ff
dmh911 <mbouazza@etud.u-pem.fr>
2021-01-18 20:13:57 +0100

    Added TP1 files

---
+++ TP1/build/CMakeFiles/TP1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP1.dir/translator.cpp.o"
+  "TP1"
+  "TP1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TP1/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mehdi/Bureau/C++/TP1
+# Build directory: /home/mehdi/Bureau/C++/TP1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
